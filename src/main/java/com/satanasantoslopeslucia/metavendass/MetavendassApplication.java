a19de5e76576c4151017bd8484e2dc74ce9a5eb7
Ana Santana <anapedra@gmail.com>
2022-11-15 16:31:00 -0300

    Projct Creatd

---
+++ src/main/java/com/satanasantoslopeslucia/metavendass/MetavendassApplication.java
@@ -0,0 +1,13 @@
+package com.satanasantoslopeslucia.metavendass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetavendassApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MetavendassApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/satanasantoslopeslucia/metavendass/MetavendassApplicationTests.java
@@ -0,0 +1,13 @@
+package com.satanasantoslopeslucia.metavendass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetavendassApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
